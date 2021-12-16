5d9959f449f90f348560b4052698e5c8941e292b
Gunvant-Sai <79575963+Gunvant-Sai@users.noreply.github.com>
2021-12-16 15:31:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+img="";
+status_="";
+object="";
+
+function setup()
+{
+    canvas = createCanvas(380,380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("model loaded");
+    status_=true;
+    objectDetector.detect(video,gotresult);
+}
+
+function gotresult(error,result)
+{
+  if(error)
+  {
+      console.log(error);
+  }
+    console.log(result);
+    object= result;
+}
+
+function preload()
+{
+    img = loadImage("dog_cat.jpg");
+}
+
+function draw()
+{
+    image(video,0,0,380,380);
+    if(status_ != "")
+    {
+      for(i= 0; i < object.length; i++)
+      {
+          r = random(255);
+          g = random(255);
+          b = random(255);
+
+          document.getElementById("status").innerHTML = "Status: Detecting Objects";
+          document.getElementById("status").innerHTML = "Number of Objects:" + object.length;
+
+          fill(r,g,b);
+          percent = floor(object[i].confidence*100);
+          text(object[i].label, object[i].x +15,object[i].y +15);
+          noFill();
+          stroke(r,g,b);
+          rect(object[i].x , object[i].y , object[i].width , object[i].height);
+      }
+    }
+}
